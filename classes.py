67c0d8fc4644d30f33c96827dac743bcae49e9b5
Hyeon-Seo yun <hyeonseo_yun@hotmail.com>
2019-06-17 11:51:50 -0500

    added more code

--- classes.py
+++ classes.py
@@ -1,4 +1,4 @@
- 
+
 class State:
     """
     A state class that represents the board state of chess
@@ -18,15 +18,29 @@ class Node:
         self.Q_ = 0.0
         self.P_ = 0.0
         self.prevNode = None # previous node variable for backtracking
-   
+
     def getChildren(self, env):
         '''
         obtains all the children nodes from interacting with the chess env
         '''
+    def simulation(self):
+        '''the
+        runs MCTS simulation once
+        '''
 
 class NN:
     """
-    A Neural Network Class that plays the role of the neural network function 
+    A Neural Network Class that plays the role of the neural network function
     in the paper "Mastering the game of Go without human knowledge"
     """
-    
+    def __init__(self):
+
+
+    def train(self, train_data):
+        '''
+        trains the network with given training data
+        '''
+    def run(self. data):
+        '''
+        runs the network with data (analogous to "testing")
+        '''
