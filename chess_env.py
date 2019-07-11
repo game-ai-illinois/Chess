4f5c420e13b8b65a523117535f807d489bd65f93
Hyeon-Seo yun <hyeonseo_yun@hotmail.com>
2019-07-10 22:34:56 -0500

    some more changes on helper functions

--- chess_env.py
+++ chess_env.py
@@ -1,5 +1,5 @@
 import chess
-import numpy as np 
+import numpy as np
 
 
 def layer_num(char):
@@ -44,7 +44,7 @@ def input_state(string):
     and creates a np array input state for AI
     the input state is (2 * 6) x 8 x 8,
     basically 12 layers of 8 by 8 board
-    where the first dimension is the layers for 
+    where the first dimension is the layers for
     each piece type of the player's pieces and
     then opponent's pieces. The order goes as:
     pawn, rook, bishop, knight, queen, king.
@@ -70,7 +70,7 @@ def is_white(string)
     """
     takes in string variable of python-chess board.fen()
     and determins if the string is in the perspective of
-    white player by looking to see if there's w in the 
+    white player by looking to see if there's w in the
     string
     """
     result = False
@@ -97,11 +97,11 @@ def text_to_position(string, is_black):
         x = 7 - x
         y = 7 - y
     return (x, y)
-    
+
 
 def array_to_text(array, is_black):
     """
-    takes in numpy array action and translates it 
+    takes in numpy array action and translates it
     into action understandable for chess env
     """
     row_increment = 0
@@ -109,9 +109,9 @@ def array_to_text(array, is_black):
     if is_black: #this is because the command changes if the action is taken by black player
         row_increment = -row_increment
         col_increment = -col_increment
-    
 
-    
+
+
 def play(board, NN, legal_moves_list, board_state_string):
     """
     takes in neural network and the list of legal moves from chess env
@@ -119,7 +119,7 @@ def play(board, NN, legal_moves_list, board_state_string):
     takes a action from the numpy array, returns numpy array equivalent
     of that action and text equivalent of that action for chess env
     """
-    legal_moves_array = np.zeros([4672]) #initialize array of legal moves 
+    legal_moves_array = np.zeros([4672]) #initialize array of legal moves
     is_black = not is_white(board_state_string)
     move_text = array_to_text(move_array, is_black)
     env_move = chess.Move.from_uci(move_text)
@@ -132,7 +132,7 @@ def play(board, NN, legal_moves_list, board_state_string):
 in our neural network, position is prioritized, then number of squares traversed,
 and then the direction of the traverse. if the order of: 64 nodes (8 by 8 chess grid),
 [1 to 7], and [N,NE,E,SE,S,SW,W,NW]
-ie, first 64 nodes all correspond to moving one square in the N direction. Then the next 64 correspond to 
+ie, first 64 nodes all correspond to moving one square in the N direction. Then the next 64 correspond to
 moving two squares in N direction, the eighth group of 64 nodes correspond to moving a square in the NE direction
 the next 8 planes (8 x 8 x 8 nodes) indicate a knight move, starting from the north north east move,
 and then rotating clockwise
@@ -149,7 +149,7 @@ def is_straight(distance_travel_x, distance_travel_y):
         return True
     else:
         return False
-    
+
 def is_knight_move(distance_travel_x, distance_travel_y):
     if is_straight(distance_travel_x, distance_travel_y):
         return False
@@ -159,10 +159,12 @@ def is_knight_move(distance_travel_x, distance_travel_y):
         else:
             return False
 
-def method():
+
+
+def squares_traveled(move_string, is_black):
     squares_travel = 0
-    start_x, start_y = text_to_position(string[0:2], is_black)
-    end_x, end_y = text_to_position(string[2:4], is_black)
+    start_x, start_y = text_to_position(move_string[0:2], is_black)
+    end_x, end_y = text_to_position(move_string[2:4], is_black)
     distance_travel_x, distance_travel_y = abs(start_x - end_x) + abs(start_y - end_y)
     if is_straight(distance_travel_x, distance_travel_y): #if straight move
         squares_travel = distance_travel_x + distance_travel_y
@@ -172,6 +174,11 @@ def method():
 
 
     else: #if diagonal move
-        squares_travel = distance_travel_x 
+        squares_travel = distance_travel_x
+    return squares_travel
 
-
+def if_promotion(move_string):
+    return_value = False
+    if len(move_string) > 4 :
+        return_value = True
+    return True
